7e7ed4c6c20772042997e2dbb157948affa67336
zx <zhangxuanjoker@sina.com>
2018-01-12 09:26:32 +0800

    引入element-UI

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//全局引入element-UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI);
+
 //生产环境的提示
 Vue.config.productionTip = false
 
