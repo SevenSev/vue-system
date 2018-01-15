b20a802294996edd04bbbf841295f921d9cce989
zx <zhangxuanjoker@sina.com>
2018-01-15 15:14:02 +0800

    add vue-resource

--- src/main.js
+++ src/main.js
@@ -4,14 +4,26 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//引入vue-resource
+import Resource from 'vue-resource'
+//引入svg-icon
+import Icon from 'vue-svg-icon/Icon.vue'
+
+//引入字体图标font-awesome
+// import 'font-awesome/css/font-awesome.css'
+  
 //全局引入element-UI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI);
 
+Vue.use(Resource);
+
+Vue.component('svg-icon', Icon);
+
 //生产环境的提示
 Vue.config.productionTip = false
-
+  
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
