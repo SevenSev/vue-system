6ee8861c3cc0f592b34740c9e90070fe4eb1fb3a
zx <zhangxuanjoker@sina.com>
2018-01-24 15:49:13 +0800

    logout features

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -19,14 +19,53 @@
 				<svg-icon name="skin" class="svgIcon"></svg-icon>
 			</div>
 			<div class="rightMenu">
-				<img src="../assets/loginMenu.gif" height="40" width="40" alt="暂时无法显示">
+				<el-dropdown trigger="click" style="height:40px;">	
+					<span class="el-dropdown-link">
+						<img src="../assets/loginMenu.gif" height="40" width="40" alt="暂时无法显示">
+						<svg-icon name="dropdown" width="15" height="15"></svg-icon>
+					</span>	
+					<el-dropdown-menu slot="dropdown">
+						<el-dropdown-item>
+							<span>首页</span>
+						</el-dropdown-item>
+
+						<a  class="itemAddress" href="https://github.com/MagicHacker/vue-system" target="_blank" >
+							<el-dropdown-item>项目地址</el-dropdown-item>
+						</a>
+
+						<el-dropdown-item divided >
+							<span v-on:click="logout">退出登录</span>
+						</el-dropdown-item>
+					</el-dropdown-menu>
+				</el-dropdown>				
 			</div>	
 		</div>				
 	</div>	
 </template>
 
 <script type="text/javascript">
-	
+	export default{
+		name:'navBar',
+		data(){
+			return {
+
+			}
+		},
+		methods:{
+		      logout(){
+		      		//删除localStorage并导航到登录页
+		      		localStorage.removeItem('username');
+		      		localStorage.removeItem('password');
+		      		this.$message({
+		      			message:'成功注销',
+		      			duration:1500
+		      		});
+		      		this.$router.push({
+		      			path:'/'
+		      		});
+		      }
+		}
+	}
 </script>
 
 <style type="text/css">
@@ -78,7 +117,7 @@
 		cursor:pointer;
 	}
 	.rightMenu{
-		width:50px;
+		width:65px;
 		height:40px;
 		vertical-align: -15px;
 		cursor:pointer;
@@ -93,5 +132,11 @@
 	.rightNav .svgIcon{
 		width:30px;
 		height:30px;
+	}
+	.itemAddress{
+		color:#606266;
+	}
+	.itemAddress:hover{
+		color:rgb(122,173,255);
 	}
 </style>
