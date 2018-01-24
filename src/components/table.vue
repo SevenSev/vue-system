830ce8c8bbe97092a1f6e07d02dc0566ec80b284
zx <zhangxuanjoker@sina.com>
2018-01-24 15:50:49 +0800

    add mock

--- src/components/table.vue
+++ src/components/table.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div>
-        <el-table v-bind:data="tableData" border style="width:100%;">
+        <el-table v-bind:data="list" border style="width:100%;">
             <el-table-column type="selection" width="55"></el-table-column>
             <el-table-column prop="date" label="日期" width="180"></el-table-column>
             <el-table-column prop="name" label="姓名" width="180"></el-table-column>
@@ -11,40 +11,44 @@
                 <template slot-scope="scope">
                     <el-button size="mini" type="">编辑</el-button>
                     <el-button size="mini" type="danger">删除</el-button>
-                </template>
+                </template> 
             </el-table-column>
         </el-table>
     </div>
 </template>
 
 <script type="text/javascript">
-    export default{
+    import Mock from 'mockjs'
+    export default{ 
       data(){
         return {
-            tableData:[
-              {
-                 date: '2016-05-02',
-                  name: '王小虎',
-                  address: '上海市普陀区金沙江路 1518 弄'
-                },
-                {
-                  date: '2016-05-04',
-                  name: '王小虎',
-                  address: '上海市普陀区金沙江路 1517 弄'
-                },
-                 {
-                  date: '2016-05-01',
-                  name: '王小虎',
-                  address: '上海市普陀区金沙江路 1519 弄'
-                }, 
-                {
-                  date: '2016-05-03',
-                  name: '王小虎',
-                  address: '上海市普陀区金沙江路 1516 弄'
-                }
-            ]
+            list:[]
         }
-      }
+      },
+
+      methods:{
+          initMock(){
+            Mock.mock('/data', {
+                'data|4-10':[
+                {
+                  'date':'@date("yyyy-mm-dd")',
+                  'name':'@cname',
+                  'address':'@city(true)'
+                }
+                ] 
+            });
+          },
+          getData(){
+            this.$http.get('/data').then(function(json){
+                this.list = json.data.data;
+                console.log(json);
+            });
+          }
+      },
+     mounted(){
+      this.initMock();
+      this.getData();
+     }
     }
 </script>
 
