d646e277da0f051783fe1b7daf130bc4b8b9913c
zx <zhangxuanjoker@sina.com>
2018-01-24 15:49:53 +0800

    children router

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -5,21 +5,21 @@
 	<div class="side-wrapper">
 		<ul class="ul-wrapper">
 			<el-menu    :unique-opened="uniqueOpened"  default-active="1" class="el-menu-vertical-demo" background-color="#304156" text-color="#fff" active-text-color="#ffd04b">
-				<router-link to="/homepage">
+				<router-link to="/layout/homepage">
 					<el-menu-item index="1">
 						<svg-icon  name="home" class="svgIcon"></svg-icon>
 						<span>首页</span>
 					</el-menu-item>					
 				</router-link>
 
-				<router-link to="/file">
+				<router-link to="/layout/file">
 					<el-menu-item index="2">
 						<svg-icon  name="document" class="svgIcon"></svg-icon>
 						<span>文档</span>
 					</el-menu-item>
 				</router-link>
 
-				<router-link to="/form">
+				<router-link to="/layout/form">
 					<el-menu-item index="3">
 						<svg-icon  name="form" class="svgIcon"></svg-icon>
 						<span>表单</span>
@@ -31,19 +31,19 @@
 						<svg-icon  name="component" class="svgIcon"></svg-icon>
 						<span>组件</span>
 					</template>
-					<router-link to="/component">
+					<router-link to="/layout/component">
 						<el-menu-item index="4-1">
 							<span>组件1</span>
 						</el-menu-item>						
 					</router-link>
 
-					<router-link to="/">
+					<router-link to="/layout/">
 						<el-menu-item index="4-2">
 							<span>组件2</span>
 						</el-menu-item>						
 					</router-link>
 
-					<router-link to="/">
+					<router-link to="/layout/">
 						<el-menu-item index="4-3">
 							<span>组件3</span>
 						</el-menu-item>						
@@ -51,14 +51,14 @@
 
 				</el-submenu>
 				
-				<router-link to="/icon">
+				<router-link to="/layout/icon">
 					<el-menu-item index="5">
 						<svg-icon  name="icon" class="svgIcon"></svg-icon>
 						<span>图标</span>
 					</el-menu-item>
 				</router-link>
 				
-				<router-link to="/zip">
+				<router-link to="/layout/zip">
 					<el-menu-item index="6">
 						<svg-icon  name="zip" class="svgIcon"></svg-icon>
 						<span>ZIP</span>
@@ -70,23 +70,23 @@
 						<svg-icon  name="chart" class="svgIcon"></svg-icon>
 						<span>图表</span>
 					</template>
-					<router-link to="/chart">
+					<router-link to="/layout/bar">
 						<el-menu-item index="7-1">
-							<span>图表1</span>
+							<span>柱状图</span>
 						</el-menu-item>
 					</router-link>
-					<router-link to="">
+					<router-link to="/layout/line">
 						<el-menu-item index="7-2">
-							<span>table</span>
+							<span>折线图</span>
 						</el-menu-item>
 					</router-link>
-					<router-link to="/">
+					<router-link to="/layout/pie">
 						<el-menu-item index="7-3">
-							<span>图表3</span>
+							<span>饼图</span>
 						</el-menu-item>
 					</router-link>
 				</el-submenu>
-				<router-link to="/table">
+				<router-link to="/layout/table">
 					<el-menu-item index="8">
 						<svg-icon  name="table" class="svgIcon"></svg-icon>
 						<span>table</span>
