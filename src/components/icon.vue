cd9426f85cc17f3ec306a9072ec6f4feae67716a
zx <zhangxuanjoker@sina.com>
2018-01-29 16:39:06 +0800

    reconstitution code with el-tooltip

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -4,114 +4,186 @@
 	<div class="iconContainer">
 		<ul>
 			<li>
+				<el-tooltip open-delay=500 content='<svg-icon  name="404" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="404" class="svgIcon"></svg-icon>
 					<p>404</p>	
-				</div>
-				<div>
-					<svg-icon  name="bug" class="svgIcon"></svg-icon>
-					<p>bug</p>	
-				</div>
+				</div>					
+				</el-tooltip>
+				<el-tooltip open-delay=500 content='<svg-icon  name="bug" class="svgIcon"></svg-icon>' placement="top-start" class="item">
+					<div>
+						<svg-icon  name="bug" class="svgIcon"></svg-icon>
+						<p>bug</p>		
+					</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="chart" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="chart" class="svgIcon"></svg-icon>
 					<p>chart</p>
-				</div>
-				<div>
-					<svg-icon  name="clipboard" class="svgIcon"></svg-icon>
-					<p>clipboard</p>
-				</div>
-				<div>
-					<svg-icon  name="component" class="svgIcon"></svg-icon>
-					<p>component</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="clipboard" class="svgIcon"></svg-icon>' placement="top-start" class="item">
+					<div>
+						<svg-icon  name="clipboard" class="svgIcon"></svg-icon>
+						<p>clipboard</p>
+					</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="component" class="svgIcon"></svg-icon>' placement="top-start" class="item">
+					<div>
+						<svg-icon  name="component" class="svgIcon"></svg-icon>
+						<p>component</p>
+					</div>					
+				</el-tooltip>
 			</li>
 			<li>
+				<el-tooltip open-delay=500 content='<svg-icon  name="dashboard" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="dashboard" class="svgIcon"></svg-icon>
 					<p>dashboard</p>
-				</div>
+				</div>					
+				</el-tooltip>	
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="document" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="document" class="svgIcon"></svg-icon>
 					<p>document</p>
-				</div>
+				</div>					
+				</el-tooltip>	
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="drag" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="drag" class="svgIcon"></svg-icon>
 					<p>drag</p>
-				</div>
+				</div>					
+				</el-tooltip>	
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="email" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="email" class="svgIcon"></svg-icon>
 					<p>email</p>
-				</div>
+				</div>					
+				</el-tooltip>	
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="example" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="example" class="svgIcon"></svg-icon>
 					<p>example</p>
-				</div>				
+				</div>					
+				</el-tooltip>	
+				
 			</li>
 			<li>
+				<el-tooltip open-delay=500 content='<svg-icon  name="eye" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="eye" class="svgIcon"></svg-icon>
 					<p>eye</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="excel" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="excel" class="svgIcon"></svg-icon>
 					<p>excel</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="icon" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="icon" class="svgIcon"></svg-icon>
 					<p>icon</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="form" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="form" class="svgIcon"></svg-icon>
 					<p>form</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="internet" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="internet" class="svgIcon"></svg-icon>
 					<p>internet</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
 			</li>
 			<li>
+				<el-tooltip open-delay=500 content='<svg-icon  name="language" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="language" class="svgIcon"></svg-icon>
 					<p>language</p>
-				</div>				
+				</div>						
+				</el-tooltip>
+			
+				<el-tooltip open-delay=500 content='<svg-icon  name="cart" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="cart" class="svgIcon"></svg-icon>
 					<p>cart</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="gear" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="gear" class="svgIcon"></svg-icon>
 					<p>gear</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="phone" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="phone" class="svgIcon"></svg-icon>
 					<p>phone</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="star" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="star" class="svgIcon"></svg-icon>
 					<p>star</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
 			</li>
 			<li>
+				<el-tooltip open-delay=500 content='<svg-icon  name="delete" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="delete" class="svgIcon"></svg-icon>
 					<p>delete</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="survey" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="survey" class="svgIcon"></svg-icon>
 					<p>survey</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="refresh" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="refresh" class="svgIcon"></svg-icon>
 					<p>refresh</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="message" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="message" class="svgIcon"></svg-icon>
 					<p>message</p>
-				</div>
+				</div>					
+				</el-tooltip>
+
+				<el-tooltip open-delay=500 content='<svg-icon  name="couple" class="svgIcon"></svg-icon>' placement="top-start" class="item">
 				<div>
 					<svg-icon  name="couple" class="svgIcon"></svg-icon>
 					<p>couple</p>
-				</div>
+				</div>					
+				</el-tooltip>
 			</li>
 		</ul>
 	</div>
@@ -119,7 +191,12 @@
 
 <script type="text/javascript">
 	export default{
-		name:'icon'
+		name:'icon',
+		data(){
+			return {
+
+			}
+		}
 	}
 </script>
 
@@ -140,6 +217,7 @@
 		display:inline-block;
 		font-size:20px;
 		text-align:center;
+		cursor:pointer;
 	}
 	.iconContainer .svgIcon{
 		width:30px;
