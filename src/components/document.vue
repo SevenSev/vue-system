4575a3edfe4a3136b2c847f24069306ba4c4dedf
zx <zhangxuanjoker@sina.com>
2018-03-12 00:20:50 +0800

    add contents

--- src/components/document.vue
+++ src/components/document.vue
@@ -1,8 +1,103 @@
 <!-- 文档页 -->
 <template>
-    <div>文档页</div>
+    <div class="document-wrapper">
+        <div class="top-wrapper">
+            <el-input v-model="input" placeholder="请输入……">
+                <el-button slot="append" icon="el-icon-search" @click="query">搜索</el-button>
+            </el-input>
+
+        </div>
+        <div class="content-wrapper">
+            <el-row :gutter="20">
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="../images/html5.jpg" >
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="../images/css.jpg">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="../images/JavaScript.jpg" >
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+            </el-row>
+            <el-row :gutter="20">
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+            </el-row>
+            <el-row :gutter="20">
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card :body-style="{padding:'0px'}">
+                        <img src="">
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
-<script type="text/javascript"></script>
+<script type="text/javascript">
+    export default{
+        data(){
+            return {
+                input:''
+            }
+        },
+        methods:{
+            query:function(){
+                alert(this.input);
+            }
+        }
+    }
+</script>
 
-<style type="text/css"></style>
+<style type="text/css">
+    div.content-wrapper img{
+        width:280px;
+        height:280px;
+    }
+
+
+</style>
